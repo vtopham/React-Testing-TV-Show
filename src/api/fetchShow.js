e7b97e2df8d404ef748c4d160ece5a00120e0198
Victoria  Topham <topham.victoria@gmail.com>
2020-04-20 14:11:56 -0700

    moved fetchShow into its own file

---
+++ src/api/fetchShow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+
+export const fetchShow = (setShow, setSeasons) => {
+    axios
+        .get(
+          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+        )
+        .then(res => {
+          setShow(res.data);
+          setSeasons(formatSeasons(res.data._embedded.episodes));
+        });
+    };
+}
+
